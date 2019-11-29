36a900d755de4640decad90fe8c9d54fbb17cbf9
Martín Sánchez Signorini <mssignorini@gmail.com>
2019-11-30 00:50:50 +0100

    Auto indented
    - 2 space indentation

--- Practica_3/development/src/main.c
+++ Practica_3/development/src/main.c
@@ -1,4 +1,4 @@
- /******************************************************************************
+/******************************************************************************
 
 *******************************************************************************/
 #include <stdio.h>
@@ -9,40 +9,40 @@
 #include "type.h"
 
 /*
-    Simple test: receives a command line such as
+Simple test: receives a command line such as
 
-        3 INT STR int
+3 INT STR int
 
-    Creates a table with thiose columns and types, opens it,
-    and prints some information about it.
+Creates a table with thiose columns and types, opens it,
+and prints some information about it.
 */
 void simple_test(int argc, char **argv) {
-    int cols = atoi(argv[0]);
-    if (cols <=0) {
-        printf("Number of columns must be positive\n");
-        exit(1);
-    }
-    if (argc-1 != cols) {
-        printf("Error: %d columns specified, %d types given\n", cols, argc-1);
-        return;
-    }
-    type_t *types = (type_t *) malloc(cols*sizeof(type_t));
-    for (int i=0; i<cols; i++) {
-        types[i] = type_parse(argv[i+1]);
-    }
-    table_create("test_tab.dat", cols, types);
-    table_t *t = table_open("test_tab.dat");
-    printf("Testing the basic functions of a table\n");
-    printf("Number of columns: %d\n", table_ncols(t));
-    printf("Types: ");
-    type_t *tps = table_types(t);
-    for (int i=0; i<table_ncols(t); i++) {
-        printf("%s  ", type_to_str(tps[i]));
-    }
-    printf("\n");
-    printf("First position: %ld (%02XH)\n", table_first_pos(t), (unsigned int) table_first_pos(t));
-    printf("Last position: %ld (%02XH)\n", table_last_pos(t), (unsigned int) table_last_pos(t));
-    table_close(t);
+  int cols = atoi(argv[0]);
+  if (cols <=0) {
+    printf("Number of columns must be positive\n");
+    exit(1);
+  }
+  if (argc-1 != cols) {
+    printf("Error: %d columns specified, %d types given\n", cols, argc-1);
+    return;
+  }
+  type_t *types = (type_t *) malloc(cols*sizeof(type_t));
+  for (int i=0; i<cols; i++) {
+    types[i] = type_parse(argv[i+1]);
+  }
+  table_create("test_tab.dat", cols, types);
+  table_t *t = table_open("test_tab.dat");
+  printf("Testing the basic functions of a table\n");
+  printf("Number of columns: %d\n", table_ncols(t));
+  printf("Types: ");
+  type_t *tps = table_types(t);
+  for (int i=0; i<table_ncols(t); i++) {
+    printf("%s  ", type_to_str(tps[i]));
+  }
+  printf("\n");
+  printf("First position: %ld (%02XH)\n", table_first_pos(t), (unsigned int) table_first_pos(t));
+  printf("Last position: %ld (%02XH)\n", table_last_pos(t), (unsigned int) table_last_pos(t));
+  table_close(t);
 }
 
 void test2() {
@@ -70,15 +70,15 @@ void test2() {
     return;
   }
   while (pos > 0) {
-      pos = table_read_record(table, pos);
-      if(pos < 0) break;
-      printf("%ld (POS)\n", pos);
-      for (i = 0; i < table_ncols(table); i++){
-          val = table_get_col(table, i);
-          print_value(stdout, tp[i], val);
-          printf("  ");
-      }
-      printf("\n");
+    pos = table_read_record(table, pos);
+    if(pos < 0) break;
+    printf("%ld (POS)\n", pos);
+    for (i = 0; i < table_ncols(table); i++){
+      val = table_get_col(table, i);
+      print_value(stdout, tp[i], val);
+      printf("  ");
+    }
+    printf("\n");
   }
   table_close(table);
 }
@@ -86,27 +86,27 @@ void test2() {
 
 int main(int argc, char **argv)
 {
-    if (argc>1) {
-        simple_test(argc-1, argv+1);
-        test2();
-    }
-    else {
-        cmdstatus *cs = c_create();
+  if (argc>1) {
+    simple_test(argc-1, argv+1);
+    test2();
+  }
+  else {
+    cmdstatus *cs = c_create();
 
-        c_key_init(cs);
-        c_print_status(cs);
-        while(1) {
-            char *cmd = c_cmd_get(40);
-            printf("\n");
-            if (!strncmp(cmd, "quit", strlen("quit"))) {
-                c_key_restore(cs);
-                printf("bye!\n");
-                exit(0);
-            }
+    c_key_init(cs);
+    c_print_status(cs);
+    while(1) {
+      char *cmd = c_cmd_get(40);
+      printf("\n");
+      if (!strncmp(cmd, "quit", strlen("quit"))) {
+        c_key_restore(cs);
+        printf("bye!\n");
+        exit(0);
+      }
 
-            c_execute(cs, cmd);
-            c_print_status(cs);
-        }
+      c_execute(cs, cmd);
+      c_print_status(cs);
     }
-    return 0;
+  }
+  return 0;
 }
