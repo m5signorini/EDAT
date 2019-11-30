94d1d733837a39bcd4b0318b44d4c6ab579ac41d
Martín Sánchez Signorini <mssignorini@gmail.com>
2019-11-30 01:09:34 +0100

    Indentation

--- Practica_3/development/src/type.c
+++ Practica_3/development/src/type.c
@@ -7,11 +7,11 @@ size_t
 value_length(type_t type, void* value) {
   switch(type) {
     case INT:
-    return sizeof(int);
+      return sizeof(int);
     case STR:
-    return (strlen((char*) value) + 1) * sizeof(char);
+      return (strlen((char*) value) + 1) * sizeof(char);
     default:
-    return 0;
+      return 0;
   }
 }
 
@@ -19,11 +19,11 @@ void
 print_value(FILE* f, type_t type, void* value) {
   switch(type) {
     case INT:
-    fprintf(f, "%d", *((int*) value));
-    break;
+      fprintf(f, "%d", *((int*) value));
+      break;
     case STR:
-    fprintf(f, "%s", (char*) value);
-    break;
+      fprintf(f, "%s", (char*) value);
+      break;
   }
 }
 
@@ -31,12 +31,12 @@ print_value(FILE* f, type_t type, void* value) {
 char *
 type_to_str(type_t type) {
   switch(type) {
-    case INT:
-    return strdup("INT");
+      case INT:
+      return strdup("INT");
     case STR:
-    return strdup("STR");
+      return strdup("STR");
     default:
-    return strdup("Invalid");
+      return strdup("Invalid");
   }
 }
 
@@ -45,11 +45,11 @@ int
 value_cmp(type_t type, void* value1, void* value2) {
   switch(type) {
     case INT:
-    return *((int*) value1) - *((int*) value2);
+      return *((int*) value1) - *((int*) value2);
     case STR:
-    return strcmp((char*) value1, (char*) value2);
+      return strcmp((char*) value1, (char*) value2);
     default:
-    return 0;
+      return 0;
   }
 }
 
@@ -68,15 +68,15 @@ void* value_parse(type_t type, char* literal) {
 
   switch(type) {
     case INT:
-    value = malloc(sizeof(int));
-    *((int*) value) = atoi(literal);
-    break;
+      value = malloc(sizeof(int));
+      *((int*) value) = atoi(literal);
+      break;
     case STR:
-    value = malloc((strlen(literal) + 1) * sizeof(char));
-    strcpy(value, literal);
-    break;
+      value = malloc((strlen(literal) + 1) * sizeof(char));
+      strcpy(value, literal);
+      break;
     default:
-    value = NULL;
+      value = NULL;
     break;
   }
 
